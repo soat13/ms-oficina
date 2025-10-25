4205b92f29c0dd3941866d208e2066b69fb4645a
priolilucas1 <priolilucas1@gmail.com>
2025-10-25 12:35:36 -0300

    fix: remove repair order status seed

--- scripts/db/migrations/2025102100001_seed_repairorder_status_status.sql
+++
@@ -1,5 +0,0 @@
--- +migrate Up
-CREATE TYPE repairorder_status AS ENUM ('received', 'in_diagnostics', 'awaiting_approval', 'approved', 'in_execution', 'finished', 'released');
-
--- +migrate Down
-DROP TYPE IF EXISTS repairorder_status;
